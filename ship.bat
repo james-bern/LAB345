8eae8da7f6674d2406a0a936d8cb95fe02b39e85
Jim <jmb15@williams.edu>
2024-02-10 19:40:00 -0500

    asdf

--- ship.bat
+++ ship.bat
@@ -1,1 +1,1 @@
-..\CS3XX\build_and_run.bat ..\CS3XX\conversation.cpp --release && copy /Y ..\CS3XX\executable.exe .\conversation.exe && woosh.bat
+vcvars.bat && ..\CS3XX\build_and_run.bat ..\CS3XX\conversation.cpp --release && copy /Y ..\CS3XX\executable.exe .\conversation.exe && woosh.bat
