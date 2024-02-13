028c3a1c050dedbcb42cf1c39899120ec3fce1a5
Jim <jmb15@williams.edu>
2024-02-12 21:24:55 -0500

    asdf

--- ship.bat
+++ ship.bat
@@ -1,1 +1,5 @@
-vcvars.bat && cd ..\CS3XX && git pull && build_and_run.bat conversation.cpp --release && cd ..\LAB345 && copy /Y ..\CS3XX\executable.exe .\conversation.exe && copy /Y ..\CS3XX\in.dxf .\in.dxf && woosh.bat
+vcvars.bat && cd ..\CS3XX && git pull && ^
+build_and_run.bat conversation.cpp --release && copy /Y .\executable.exe ..\LAB345\conversation.exe && ^
+build_and_run.bat viewer.cpp --release && copy /Y .\executable.exe ..\LAB345\viewer.exe && ^
+copy /Y .\splash.dxf ..\LAB345\splash.dxf && ^
+cd ..\LAB345 && woosh.bat
