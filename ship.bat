80171c35073f50146bef77b152a9ce423efe8f79
owl <jmb15@williams.edu>
2024-07-01 16:28:22 -0400

    asdf

--- ship.bat
+++ ship.bat
@@ -1,6 +1,7 @@
 del converstaion.exe && del viewer.exe && ^
-vcvars.bat && cd ..\CS3XX && git pull && ^
-build_and_run.bat conversation.cpp --release && copy /Y .\executable.exe ..\LAB345\conversation.exe && ^
-build_and_run.bat viewer.cpp --release && copy /Y .\executable.exe ..\LAB345\viewer.exe && ^
+cd ..\CS3XX && git pull && ^
+build_and_run.bat main.cpp --ship && copy /Y .\executable.exe ..\LAB345\conversation.exe && ^
 copy /Y .\splash.dxf ..\LAB345\splash.dxf && ^
-cd ..\LAB345 && woosh.bat
+cd ..\LAB345 && poosh.bat
+
+rem build_and_run.bat viewer.cpp --release && copy /Y .\executable.exe ..\LAB345\viewer.exe && ^
